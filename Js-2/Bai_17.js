0d05aaee50ef0a2f8ff45dfe74b23d89aa615def
Cuongle99 <lecuong270999@gmail.com>
2022-09-15 09:51:57 +0700

    change 2

--- Js-2/Bai_17.js
+++ Js-2/Bai_17.js
@@ -1,32 +1,3 @@
-const isMax = s => {
-    return Math.max(...s);
-}
-const isMin = s => {
-    return Math.min(...s);
-}
-
-const isMax2 = s => {
-    let max = s[0]
-    for (let i = 1; i < s.length; i++) {
-        if(s[i] > max) {
-            max = s[i]
-        }
-        
-    }
-    return max
-}
-
-const isMin2 = s => {
-    let min = s[0]
-    for (let i = 1; i < s.length; i++) {
-        if(s[i] < min) {
-            min = s[i]
-        }
-        
-    }
-    return min
-}
-
 const isMax3 = (a, b, c) => {
     let max = a;
     if (b > max) {
