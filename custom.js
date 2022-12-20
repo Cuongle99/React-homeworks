f5f2d7b2a713374256bc52725a808641a0a4be9e
Cuongle99 <110615128+Cuongle99@users.noreply.github.com>
2022-12-20 16:27:04 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -15,3 +15,21 @@ $(function () {
     });
     
 });
+
+
+$(document).ready(function () {
+    var offSetImage = $(".banner_img").offset();
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        var space = -(scrollTop + 400 - offSetImage.top)/3
+        if (space < -400) {
+            space = -400
+        } else if(kc > 0) {
+            space = 0
+        }
+
+        $(".banner_img").css({
+            "background-position-y": space + "px",
+        });
+    });
+});
